f91080ece8bc076e332e2efb00d8dd7cd355ca28
Paolo <pao.calabrese1@stud.uniroma3.it>
2014-12-24 21:32:45 +0100

    user JPA entity class added (generated from table). removed old user class, added notation and corrected data type from BigInteger to long.

--- Mobike-SRV/src/model/User.java
+++ Mobike-SRV/src/model/User.java
@@ -1,154 +1,107 @@
-/**
- * 
- */
 package model;
 
- import java.util.List;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.*;
+
 
 /**
- * @author Paolo
- *
+ * The persistent class for the users database table.
+ * 
  */
-public class User {
-	
-	/**
-	 * @param id
-	 */
-	
+@Entity
+@Table(name="users")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-	private long userId;
-	private String name;
-	private String surname;
-	private String username;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(unique=true, nullable=false)
+	private long id;
+
+	@Column(nullable=false, length=50)
 	private String email;
+
+	@Column(nullable=false, length=50)
+	private String name;
+
+	@Column(nullable=false, length=50)
 	private String password;
-	private List<Integer> pathsId;
+
+	@Column(length=50)
+	private String surname;
+
+	@Column(nullable=false, length=50)
+	private String username;
 	
-	
-	public User(int id) {
-		super();
-		this.userId = id;
-	}
-	
-	/**
-	 * @param id
-	 * @param name
-	 * @param surname
-	 * @param username
-	 * @param email
-	 * @param password
-	 * @param pathsId
-	 */
-	public User(int id, String name, String surname, String username,
-			String email, String password, List<Integer> pathsId) {
-		super();
-		this.name = name;
-		this.surname = surname;
-		this.username = username;
-		this.email = email;
-		this.password = password;
-		this.pathsId = pathsId;
+	@Transient
+	private List<Path> Paths;
+
+	public User() {
 	}
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
+	public long getId() {
+		return this.id;
 	}
-	
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
+
+	public void setId(long id) {
+		this.id = id;
 	}
-	
-	/**
-	 * @return the surname
-	 */
-	public String getSurname() {
-		return surname;
-	}
-	
-	/**
-	 * @param surname the surname to set
-	 */
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	
-	/**
-	 * @return the username
-	 */
-	public String getUsername() {
-		return username;
-	}
-	
-	/**
-	 * @param username the username to set
-	 */
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	/**
-	 * @return the email
-	 */
+
 	public String getEmail() {
-		return email;
+		return this.email;
 	}
-	
-	/**
-	 * @param email the email to set
-	 */
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
+
+	public String getName() {
+		return this.name;
 	}
-	
-	/**
-	 * @param password the password to set
-	 */
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	/**
-	 * @return the pathsId
-	 */
-	public List<Integer> getPathsId() {
-		return pathsId;
+
+	public String getSurname() {
+		return this.surname;
 	}
-	
-	/**
-	 * @param pathsId the pathsId to set
-	 */
-	public void setPathsId(List<Integer> pathsId) {
-		this.pathsId = pathsId;
+
+	public void setSurname(String surname) {
+		this.surname = surname;
 	}
-	
-	/**
-	 * @return the id
-	 */
-	public long getId() {
-		return userId;
+
+	public String getUsername() {
+		return this.username;
 	}
-	
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(long id) {
-		userId = id;
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	
-	
-	
+
+	/**
+	 * @return the paths
+	 */
+	public List<Path> getPaths() {
+		return Paths;
+	}
+
+	/**
+	 * @param paths the paths to set
+	 */
+	public void setPaths(List<Path> paths) {
+		Paths = paths;
+	}
 
 }
