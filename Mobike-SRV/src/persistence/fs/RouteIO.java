85097a0a30d961c967f6ce592e49f2bc98e78920
thespeedfreak <bvispi@gmail.com>
2015-01-20 15:55:11 +0100

    added javadoc routeio

--- Mobike-SRV/src/persistence/fs/RouteIO.java
+++ Mobike-SRV/src/persistence/fs/RouteIO.java
@@ -4,10 +4,25 @@ package persistence.fs;
 
 import persistence.exception.FilesystemException;
 
+/**
+ * @author br1
+ *
+ */
 public interface RouteIO {
 	
+	/**
+	 * @param content
+	 * @param filename
+	 * @return
+	 * @throws FilesystemException
+	 */
 	public String write(String content,String filename) throws FilesystemException;
 	
+	/**
+	 * @param filepath
+	 * @return
+	 * @throws FilesystemException
+	 */
 	public String read(String filepath) throws FilesystemException;
 
 }
--- Mobike-SRV/src/persistence/jpa/SingletonEMF.java
+++ Mobike-SRV/src/persistence/jpa/SingletonEMF.java
@@ -3,20 +3,37 @@ package persistence.jpa;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * @author bruno, paolo, marco, andrea
+ * @version 1.0
+ */
 public final class SingletonEMF {
 
+	
 	private static final EntityManagerFactory emfInstance =
 		Persistence.createEntityManagerFactory("Mobike-SRV");
 	
+	/**
+	 * default empty constructor
+	 */
 	private SingletonEMF(){}
 	
 	
+	
+	/**
+	 * @return
+	 */
 	public static EntityManagerFactory get(){
 		return emfInstance;
 	}
 	
 	
-	  public static void closeEntityManager() {
+	
+	  /**
+	 * 
+	 */
+	public static void closeEntityManager() {
 	        emfInstance.close();
 	    }
+	
 }
