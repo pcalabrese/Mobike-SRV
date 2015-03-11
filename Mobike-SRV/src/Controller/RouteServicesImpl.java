14fb1389e211f66fc6c3f1f8f2e9d92702fd18d5
thespeedfreak <bvispi@gmail.com>
2015-03-11 13:49:09 +0100

    Changed Json serialization tool from GSON to the Jackson POJO mapping for RouteServices

--- Mobike-SRV/src/Controller/RouteServicesImpl.java
+++ Mobike-SRV/src/Controller/RouteServicesImpl.java
@@ -8,7 +8,7 @@ import persistence.exception.*;
 import persistence.mysql.RouteMySQL;
 import persistence.fs.*;
 import model.Route;
-
+import model.Views;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -17,19 +17,21 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.*;
 /** 
  * Main RESTful Controller for HTTP POST and GET requests
  * @author Paolo, Bruno, Marco, Andrea
- * @version 2.0
+ * @version 3.0
  * @see RouteServices RouteServices: Interface implemented by this class
  * @see RouteMySQL RouteMySQL: SQL DAO Class
- * 
+ *
  */
 @Path("/routes")
 	public class RouteServicesImpl implements RouteServices {
@@ -114,19 +116,58 @@ import com.google.gson.*;
 		@Path("/retrieve/{Id}")
 		@Produces(MediaType.APPLICATION_JSON)
 		@Override
-		public String getRoute(@PathParam("Id") Long id) {
+		public Response getRoute(@PathParam("Id") Long id) {
+			
 			System.out.println(id);
+			String json = null;
 			Route route = null;
 			RouteRepository routeREP = new RouteMySQL();
+			ObjectMapper objectMapper = new ObjectMapper();
+			String gpxString;
+			RouteIO fsrepo = new GpxIO();
+			
+			//take route from db
 			try{
 			route = routeREP.routeFromId(id);
 			}
 			catch(Exception e){
 				throw new UncheckedPersistenceException("Error accessing routes database");
 			}
-			Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
+			
+			// check route!=null -> id exists in the DB
+			if(route!=null){
+			//take gpxString from file on server
+				try{
+				gpxString = fsrepo.read(route.getUrl().substring(8));
+				}	catch (Exception e){
+						throw new UncheckedFilesystemException("Error reading gpx file",e);
+					}
+			
+			//set the gpxString in the Route bean
+			route.setGpxString(gpxString);
+			
+			//set the view to return just the json fields for the visualization and exclude the default view
+			objectMapper.setConfig(objectMapper.getSerializationConfig().withView(Views.ItineraryDetailView.class));
+			objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);
+
+			//starting JSON serialization
+				try {
+				json = objectMapper.writerWithView(Views.ItineraryDetailView.class).writeValueAsString(route);
+				} 	catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+					}
+			
+			/*Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
 			System.out.println(route.getUrl());
-			return gson.toJson(route, Route.class);
+			return gson.toJson(route, Route.class);*/
+			return Response.ok(json, MediaType.APPLICATION_JSON).build();
+			}
+			// route=null -> id doesnt exist in the DB
+			else {
+				return Response.status(404).build();
+			}
+
 		}
 
 		/**
@@ -140,24 +181,38 @@ import com.google.gson.*;
 		@Path("/retrieveall")
 		@Produces(MediaType.APPLICATION_JSON)
 		@Override
-		public String retrieveAllRoutes() {
+		public Response retrieveAllRoutes() {
 			
 			List<Route> allRoutes = null;
+			ObjectMapper objectMapper = new ObjectMapper();
+			String json = null;
+			
 			try {
 				allRoutes = routeRep.getAllRoutes();
 			} catch (Exception e) {
 				throw new UncheckedPersistenceException("Error accessing route database",e);
 			}
-			ObjectMapper objectMapper = new ObjectMapper();
-			String json = null;
+			// check if allRoutes has been populated, with isEmpty -> return 404
+			if(!(allRoutes.isEmpty())){
 			
-			try {
-				json = objectMapper.writeValueAsString(allRoutes);
-			} catch (JsonProcessingException e) {
+			//set the view to return just the json fields for the visualization and exclude the default view
+			objectMapper.setConfig(objectMapper.getSerializationConfig().withView(Views.ItineraryGeneralView.class));
+			objectMapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);
+			
+				try {
+					json = objectMapper.writerWithView(Views.ItineraryGeneralView.class).writeValueAsString(allRoutes);
+				} 	catch (JsonProcessingException e) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				e.printStackTrace();					
+					}
+			
+			return Response.ok(json, MediaType.APPLICATION_JSON).build(); 
 			}
-			return json;
+			
+			else {
+				return Response.status(404).build();
+			}
+			
 		}
 
 		/**
